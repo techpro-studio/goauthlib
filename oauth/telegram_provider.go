3da649f98dd92addb4e87f29b018ae23ed6cffb2
alex <alex@techpro.studio>
2024-12-01 21:42:38 +0300

    telegram appended json data.

--- oauth/telegram_provider.go
+++ oauth/telegram_provider.go
@@ -35,15 +35,15 @@ func isJSONString(s string) bool {
 	return json.Unmarshal([]byte(s), &js) == nil
 }
 
-func VerifyTelegramData(values url.Values, botToken string) error {
+func VerifyTelegramData(values map[string]any, botToken string) error {
 	// Extract the 'hash' parameter
-	receivedHash := values.Get("hash")
-	if receivedHash == "" {
-		return errors.New("No hash parameter provided")
+	receivedHash, ok := values["hash"].(string)
+	if !ok || receivedHash == "" {
+		return errors.New("no hash parameter provided")
 	}
 
 	// Remove 'hash' from the parameters to avoid including it in the data check string
-	valuesCopy := url.Values{}
+	valuesCopy := map[string]any{}
 	for key, val := range values {
 		if key != "hash" {
 			valuesCopy[key] = val
@@ -61,16 +61,23 @@ func VerifyTelegramData(values url.Values, botToken string) error {
 	var dataCheckStrings []string
 	for _, key := range keys {
 		// Join multiple values for the same key (if any)
-		joinedValues := strings.Join(valuesCopy[key], ",")
-		dataCheckStrings = append(dataCheckStrings, fmt.Sprintf("%s=%s", key, joinedValues))
+		var stringValue string
+		floatValue, floatOk := valuesCopy[key].(float64)
+		if floatOk {
+			stringValue = strconv.Itoa(int(floatValue))
+		} else {
+			stringValue = valuesCopy[key].(string)
+		}
+
+		dataCheckStrings = append(dataCheckStrings, fmt.Sprintf("%s=%s", key, stringValue))
 	}
 	dataCheckString := strings.Join(dataCheckStrings, "\n")
 
 	var secretKey []byte
 
 	// If 'user' parameter exists and is a JSON string, it's from WebAppData
-	userValue := valuesCopy.Get("user")
-	if userValue != "" && isJSONString(userValue) {
+	userValue, ok := valuesCopy["user"].(string)
+	if ok && userValue != "" && isJSONString(userValue) {
 		// Compute secret key for WebAppData
 		secretKey = computeHmacSha256([]byte("WebAppData"), []byte(botToken))
 	} else {
@@ -104,41 +111,69 @@ func getStringSafe(m map[string]interface{}, key string) string {
 	return "" // or a default value
 }
 
+func extractTelegramParameters(infoToken string) (map[string]any, error) {
+	result := map[string]any{}
+	err := json.Unmarshal([]byte(infoToken), &result)
+	if err == nil {
+		return result, nil
+	}
+	urlParams, err := url.ParseQuery(infoToken)
+	for key, value := range urlParams {
+		result[key] = value[0]
+	}
+	return result, nil
+}
+
+type UserData struct {
+	Id        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Username  string `json:"username"`
+}
+
+const kFirstName = "first_name"
+const kLastName = "last_name"
+const kUserName = "username"
+const kId = "id"
+
+func mapUserData(usr map[string]any) UserData {
+	var id string
+	id, ok := usr[kId].(string)
+	if !ok {
+		id = strconv.Itoa(int(usr[kId].(float64)))
+	}
+	return UserData{
+		Id:        id,
+		FirstName: getStringSafe(usr, kFirstName),
+		LastName:  getStringSafe(usr, kLastName),
+		Username:  getStringSafe(usr, kUserName),
+	}
+}
+
 func (t *TelegramProvider) GetInfoByToken(ctx context.Context, infoToken string) (*ProviderResult, error) {
 
 	if strings.HasPrefix(infoToken, "?") {
 		infoToken = infoToken[1:]
 	}
 
-	params, err := url.ParseQuery(infoToken)
+	params, err := extractTelegramParameters(infoToken)
+
 	if err != nil {
 		return nil, err
 	}
 
-	var firstName string
-	var lastName string
-	var userName string
-	var id string
-	user := params.Get("user")
-	const kFirstName = "first_name"
-	const kId = "id"
-	const kLastName = "last_name"
-	const kUserName = "username"
-	if user == "" {
-		id = params.Get(kId)
-		firstName = params.Get(kFirstName)
-		lastName = params.Get(kLastName)
-		userName = params.Get(kUserName)
-	} else {
+	user, userOk := params["user"].(string)
+
+	var userData UserData
+	if userOk && user != "" {
 		var usr map[string]any
 		err := json.Unmarshal([]byte(user), &usr)
 		if err != nil {
 			return nil, err
 		}
-		firstName = getStringSafe(usr, kFirstName)
-		lastName = getStringSafe(usr, kLastName)
-		userName = getStringSafe(usr, kUserName)
-		id = strconv.Itoa(int(usr[kId].(float64)))
+		userData = mapUserData(usr)
+	} else {
+		userData = mapUserData(params)
 	}
 
 	err = VerifyTelegramData(params, t.botToken)
@@ -148,14 +183,14 @@ func (t *TelegramProvider) GetInfoByToken(ctx context.Context, infoToken string)
 	}
 
 	return &ProviderResult{
-		ID:    id,
+		ID:    userData.Id,
 		Type:  "telegram",
 		Email: "",
 		Phone: "",
 		Raw: map[string]interface{}{
-			kFirstName:                      firstName,
-			kLastName:                       lastName,
-			fmt.Sprintf("tg_%s", kUserName): userName,
+			kFirstName:                      userData.FirstName,
+			kLastName:                       userData.LastName,
+			fmt.Sprintf("tg_%s", kUserName): userData.Username,
 		},
 	}, nil
 }
